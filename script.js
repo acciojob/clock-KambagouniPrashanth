ad45ca80ca9863330eeeccdac3bda7b5ca763ca7
Kambagouni Prashanth <117985368+KambagouniPrashanth@users.noreply.github.com>
2023-06-25 12:01:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+//your JS code here. If required.
+const p=document.getElementById("timer")
+
+let today=new Date();
+// const arr=[]
+// arr.push(today.getDate())
+let day=today.getDate();
+let month=today.getMonth();
+let year=today.getFullYear()
+let hour=today.getHours();
+let minute=today.getMinutes()
+let second=today.getSeconds();
+
+
+p.innerText=day+"/"+month+"/"+year+" ,"+hour+":"+minute+":"+second;
+
+
+
